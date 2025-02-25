4a0f8b983ec185605c7f3ae0b08aa941bf0f8eec
adam.justin <adam.justin@portabo.cz>
2025-02-25 10:18:22 +0100

    neco malo delam i na ty hodine xd

---
+++ oop.py
@@ -0,0 +1,24 @@
+lst = [2,3,1,4]
+
+sorted(lst) #funkce
+lst.sort() # metoda
+
+x = 42
+print(x.__class__)
+
+x = 42
+print(type(x)) # calss int
+
+y = "Hello"
+print(type(y)) # class string
+
+z = [1, 2, 3]
+print(type(z)) # class list
+
+x = 42
+print(dir(x))
+print(dir(type(x)))
+
+x = 42
+print(x.__class__)
+print(x.__class__.__name__)
